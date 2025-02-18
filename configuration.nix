d2559f1829838f9b937bfe2467530387720b92ae
DemyCode <verycols@gmail.com>
2025-02-18 14:45:29 +0100

    fix: ACME certification with email

--- configuration.nix
+++ configuration.nix
@@ -33,8 +33,34 @@ in
     pkgs.curl
     pkgs.gitMinimal
     pkgs.gitea
-    pkgs.just # for justfile
+    pkgs.just
+    pkgs.nginx
   ];
+  services.nginx.package = pkgs.nginxStable.override { openssl = pkgs.libressl; };
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    # other Nginx options
+    virtualHosts."git.mehdibektaoui.com" =  {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+        proxyWebsockets = true; # needed if you need to use WebSocket
+        extraConfig =
+          # required when the target is also TLS server with multiple hosts
+          "proxy_ssl_server_name on;" +
+          # required when the server wants to use HTTP Authentication
+          "proxy_pass_header Authorization;"
+          ;
+      };
+    };
+  };
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = secrets.acme_email;
+  };
   users.users.gitea = {
     extraGroups  = [ "wheel" "networkmanager" ];
     openssh.authorizedKeys.keys  = [ secrets.gitea ];
