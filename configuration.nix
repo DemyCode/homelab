3db8e9b3e7f13c029b6f67598d821481c5b3859b
DemyCode <verycols@gmail.com>
2025-02-18 15:35:01 +0100

    Revert "fix: lets go"
    This reverts commit ab8430e37928ccf9031cbd1ae62116f1698c625e.

--- configuration.nix
+++ configuration.nix
@@ -46,12 +46,13 @@ in
       enableACME = true;
       forceSSL = true;
       locations."/" = {
-        proxyPass = "http://localhost:3000";
+        proxyPass = "http://127.0.0.1:3000";
         proxyWebsockets = true; # needed if you need to use WebSocket
         extraConfig =''
             proxy_ssl_server_name on;
             proxy_pass_header Authorization;
             client_max_body_size 512M;
+            proxy_pass http://localhost:3000;
             proxy_set_header Connection $http_connection;
             proxy_set_header Upgrade $http_upgrade;
             proxy_set_header Host $host;
