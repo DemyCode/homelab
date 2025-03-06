ec8cc2595eb809146a4dc2497bf6eefb0bf13f2f
DemyCode <verycols@gmail.com>
2025-03-06 17:46:33 +0100

    fix: removing useless stuff

--- flake.nix
+++ flake.nix
@@ -2,14 +2,12 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
   inputs.wireguard-mesh-coordinator.url = "github:DemyCode/wireguard-mesh-coordinator";
 
   outputs =
     {
       nixpkgs,
       disko,
-      nixos-facter-modules,
       wireguard-mesh-coordinator,
       ...
     }:
