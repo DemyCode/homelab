3200b4b9d6d109a1c7478d3b1971a940154b07cd
DemyCode <verycols@gmail.com>
2025-10-14 21:34:43 +0200

    feat: deleting hardware configuration

--- yolab-manager/src/yolab_manager/main.py
+++ yolab-manager/src/yolab_manager/main.py
@@ -1,22 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+import os
+from pathlib import Path
 
 app = FastAPI()
 
 
-@app.get("/health")
+@app.get("/update")
 def health():
-    return {"status": "success"}
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    git_repo_path = Path("/deployments")
+    if git_repo_path.exists() and git_repo_path.is_dir():
+        os.chdir(git_repo_path)
+        os.system("git pull")
+    else:
+        os.system("git clone https://github.com/DemyCode/homelab /deployments")
 
 
 def serve():
